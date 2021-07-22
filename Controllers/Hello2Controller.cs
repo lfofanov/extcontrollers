68577634569edc58235ffd0277cc41edbacad5b3
Leonid Fofanov <lfofanov@hotmail.com>
2021-07-22 14:28:40 -0400

    Add project files.

---
+++ Controllers/Hello2Controller.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ControllersExt
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class Hello2Controller : ControllerBase
+    {
+        [HttpGet("time")]
+        public string GetCurrentTime()
+        {
+            return $"Hello from external {this.GetType().Name}. Current time is: {DateTime.Now}";
+        }
+
+
+    }
+}
